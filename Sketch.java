f281e723490177b4c12c4e9eb0e24c03d10d1fb8
Stephen123 <Stephen.Liu25@ycdsbk12.ca>
2023-10-12 22:56:46 -0400

    Yes

--- Sketch.java
+++ Sketch.java
@@ -1,3 +1,5 @@
+import java.time.LocalTime;
+
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
@@ -8,7 +10,8 @@ public class Sketch extends PApplet {
    */
   public void settings() {
 	// put your size call here
-    size(400, 400);
+  
+    size(500, 500);
   }
 
   /** 
@@ -16,21 +19,51 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(255, 179, 0);
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+  // Declare variables
+    float flowerX = random(0, width);
+    float flowerY = random(0, height);
+    float petalSize = random(50, 250);
+    float petalDistance = petalSize / 2;
+
+  // Draw top-left petal
+    fill(255, 3, 3);
+    ellipse(flowerX - petalDistance, flowerY - petalDistance, petalSize, petalSize);
+
+  // Draw top-right petal
+    fill(255, 3, 3);
+    ellipse(flowerX + petalDistance, flowerY + petalDistance, petalSize, petalSize);
+
+  // Draw bottom-left petal
+    fill(255, 3, 3);
+    ellipse(flowerX - petalDistance, flowerY + petalDistance, petalSize, petalSize);
+
+  // Draw bottom-right petal
+    fill(255, 3, 3);
+    ellipse(flowerX + petalSize, flowerY + petalDistance, petalSize, petalSize);
+
+      // Draw flower center
+    fill(10, 10, 10);
+    ellipse(flowerX, flowerY, petalSize, petalSize);
+
+  // If statement
+
+    if(flowerX > 200){
+        background(66, 135,245);
+    }
+   
+    // Display current time
+
+      System.out.println(java.time.LocalTime.now());
+
+    }
   }
-  
-  // define other methods down here.
-}
+
+
